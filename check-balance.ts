21b67285d27a6e76620c336100403414e36392cf
henghasaint <mercuryerflyer101@gmail.com>
2024-03-15 15:22:15 +0800

    generate 2nd keypair

--- check-balance.ts
+++ check-balance.ts
@@ -1,11 +1,11 @@
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import "dotenv/config"
-import { getKeypairFromEnvironment  } from "@solana-developers/helpers";
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+const keypair = getKeypairFromEnvironment("SECRET_KEY1");
 const publicKey = keypair.publicKey.toBase58()
 const connection = new Connection(clusterApiUrl("devnet"));
 const address = new PublicKey(publicKey);
 const balance = await connection.getBalance(address);
 const balanceInSol = balance / LAMPORTS_PER_SOL;
-console.log(`The balance of the account at ${address} is ${balanceInSol} SOL`); 
+console.log(`The balance of the account at ${address} is ${balanceInSol} SOL`);
 console.log(`✅ Finished!`)
