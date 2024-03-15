9c3e7b09cd865c7ef6c74c59f93c4f493d27d5f5
henghasaint <mercuryerflyer101@gmail.com>
2024-03-15 16:24:28 +0800

    work now

--- transfer.ts
+++ transfer.ts
@@ -3,7 +3,8 @@ import {
     Transaction,
     SystemProgram,
     sendAndConfirmTransaction,
-    PublicKey
+    PublicKey,
+    LAMPORTS_PER_SOL
 } from "@solana/web3.js";
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
@@ -15,21 +16,42 @@ if (!suppliedToPubkey) {
     process.exit(1);
 }
 const senderKeypair = getKeypairFromEnvironment("SECRET_KEY1");
+console.log(`senderKeypair PubKey: ${senderKeypair.publicKey}`);
 console.log(`suppliedToPubkey: ${suppliedToPubkey}`);
 const toPubkey = new PublicKey(suppliedToPubkey);
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+const address = new PublicKey(senderKeypair.publicKey);
+const balance = await connection.getBalance(address);
+const balanceInSol = balance / LAMPORTS_PER_SOL;
+console.log(`The balance of ${address.toBase58()} is ${balanceInSol}`);
+
 console.log(`✅ Loaded our own keypair, the destination public key, and connected to Solana`);
 console.log(
     `✅ Loaded our own keypair, the destination public key, and connected to Solana`
 );
 const transaction = new Transaction();
-const LAMPORTS_TO_SEND = 5000;
+
+const LAMPORTS_TO_SEND = 6;
+
 const sendSolInstruction = SystemProgram.transfer({
     fromPubkey: senderKeypair.publicKey,
     toPubkey,
     lamports: LAMPORTS_TO_SEND,
 });
+
 transaction.add(sendSolInstruction);
-const signature = await sendAndConfirmTransaction(connection, transaction, [senderKeypair]);
-console.log(`💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `);
-console.log(`Transaction signature is : ${signature}`);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+]);
+
+console.log(
+    `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `
+);
+console.log(`Transaction signature is ${signature}!`);
+
+
+// In Solana, accounts need to maintain a minimum balance to remain rent - exempt.
+// This minimum balance is determined by the account's data size and the current rent exemption threshold. 
+// If an account's balance falls below this threshold, it is not rent-exempt, and the network can purge it.
